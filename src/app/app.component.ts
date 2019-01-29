bcc1232dfcfffa792c9056ec3ff67664ba8a9d80
AlanOsvaldo1 <alan.mayorga@alumno.uttehuacan.edu.mx>
2019-01-29 13:53:35 -0600

    alan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { RegistroJugadorPage } from '../pages/registro-jugador/registro-jugador';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = RegistroJugadorPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
