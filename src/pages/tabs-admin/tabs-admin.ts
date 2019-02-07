f878d86cc2ef1ea3e77495815745f08472f531f1
AlanOsvaldo1 <alan.mayorga@alumno.uttehuacan.edu.mx>
2019-02-06 18:16:51 -0600

    Alan

--- src/pages/tabs-admin/tabs-admin.ts
+++ src/pages/tabs-admin/tabs-admin.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { RegistrarsePage } from '../registrarse/registrarse';
 import { AdministradorPage } from '../administrador/administrador';
-import { TabsAdminPage } from '../tabs-admin/tabs-admin';
 
 /**
- * Generated class for the LoginPage page.
+ * Generated class for the TabsAdminPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -13,24 +11,18 @@ import { TabsAdminPage } from '../tabs-admin/tabs-admin';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: 'page-tabs-admin',
+  templateUrl: 'tabs-admin.html',
 })
-export class LoginPage {
+export class TabsAdminPage {
+
+  tab1 = AdministradorPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-  
-  irRegistro(){
-    this.navCtrl.push(RegistrarsePage);
-  }
-
-  public irHomeAdmin(){
-    this.navCtrl.setRoot(TabsAdminPage);
+    console.log('ionViewDidLoad TabsAdminPage');
   }
 
 }
