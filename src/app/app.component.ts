45b58f63568871093051da83dfc78f68820d2518
Juan Manuel <cjuan.cn@gmail.com>
2019-02-07 07:04:39 -0600

    Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {RegistroTorneoPage} from '../pages/registro-torneo/registro-torneo';
@@ -13,12 +13,16 @@ import { ConfiguracionPage } from '../pages/configuracion/configuracion';
 import { RegistroArbitrosPage } from '../pages/registro-arbitros/registro-arbitros';
 import { RegistroLigaPage } from '../pages/registro-liga/registro-liga';
 import { AdministradorPage} from '../pages/administrador/administrador';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
 
-  rootPage:any = LoginPage;
+export class MyApp {
+@ViewChild(Nav) nav: Nav;
+
+  rootPage:any = RegistroJugadorPage;
+  pages: Array<{title: String, Component: any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -26,6 +30,20 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.pages=[{title: 'Home', Component: HomePage},
+                  {title: 'Jugadores', Component: JugadoresPage},
+                  {title: 'Registro de Arbitros', Component: RegistroArbitrosPage},
+                  {title: 'Registro de Torneos', Component: RegistroTorneoPage},
+                  {title: 'Registro de Torneos', Component: RegistroTorneoPage}];
     });
+
+    
+  }
+
+  
+
+  public openPage(page){
+    this.nav.setRoot(page.Component);
   }
 }
