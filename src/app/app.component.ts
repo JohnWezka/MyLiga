43314f831d1e7fc417742eabd0aa08adce8e46ff
AlanOsvaldo1 <alan.mayorga@alumno.uttehuacan.edu.mx>
2019-02-07 23:51:15 -0600

    Alan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,52 +13,26 @@ import { ConfiguracionPage } from '../pages/configuracion/configuracion';
 import { RegistroArbitrosPage } from '../pages/registro-arbitros/registro-arbitros';
 import { RegistroLigaPage } from '../pages/registro-liga/registro-liga';
 import { AdministradorPage} from '../pages/administrador/administrador';
-
 import { RegistroUsuariosPage } from '../pages/registro-usuarios/registro-usuarios';
 import { ElegirLigaPage } from '../pages/elegir-liga/elegir-liga';
 import { JugadorInfoPage } from '../pages/jugador-info/jugador-info';
-<<<<<<< HEAD
-
 import { RegistroPartidosPage } from '../pages/registro-partidos/registro-partidos';
-
-
-
 import { TablaDePuntosPage } from '../pages/tabla-de-puntos/tabla-de-puntos';
 import { TablaPartidosPage } from '../pages/tabla-partidos/tabla-partidos';
 import { TabaPartidosAdminPage } from '../pages/taba-partidos-admin/taba-partidos-admin';
-
-=======
-<<<<<<< HEAD
 import { EquiposAdminPage } from '../pages/equipos-admin/equipos-admin';
-=======
 import { RegistroMarcadorPage } from '../pages/registro-marcador/registro-marcador';
-import { TablaDePuntosPage } from '../pages/tabla-de-puntos/tabla-de-puntos';
->>>>>>> c76e3f5dff896b87097ca0ae636e553d7ad8b96d
->>>>>>> c47817f87f6a2f4d94515747d395d261a2947c46
 @Component({
   templateUrl: 'app.html'
 })
 
 export class MyApp {
-<<<<<<< HEAD
-
-<<<<<<< HEAD
 @ViewChild(Nav) nav: Nav;
 
 
-  rootPage:any = TabaPartidosAdminPage;
+  rootPage:any = TabPage;
   
   pages: Array<{title: String, Component: any}>;
-
-
-=======
-  rootPage:any = EquiposAdminPage;
-
-
-=======
-  rootPage:any = AdministradorPage ;
->>>>>>> c76e3f5dff896b87097ca0ae636e553d7ad8b96d
->>>>>>> c47817f87f6a2f4d94515747d395d261a2947c46
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
