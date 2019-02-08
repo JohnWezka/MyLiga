fdceb723d6d9e89b0bab5bb8a3f020a4f8ffdf0b
chypher-Paradox <lokyfactormiedo@gmail.com>
2019-02-07 23:00:55 -0600

    marco

---
+++ src/pages/equipos/equipos.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, NavPush } from 'ionic-angular';
+import { JugadoresPage } from '../jugadores/jugadores';
+
+/**
+ * Generated class for the EquiposPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-equipos',
+  templateUrl: 'equipos.html',
+})
+export class EquiposPage {
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public toJugadores(){
+    this.navCtrl.push(JugadoresPage);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EquiposPage');
+  }
+
+}
---
+++ src/pages/tab/tab.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { EquiposPage } from '../equipos/equipos';
+/**
+ * Generated class for the TabPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tab',
+  templateUrl: 'tab.html',
+})
+export class TabPage {
+
+  tab1 = HomePage;
+  tab2 = EquiposPage;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabPage');
+  }
+
+}
